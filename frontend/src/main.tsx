164cd79b8bae8f451ffb9da2a6df62ec448a46f0
Raphael <gld141@mail.usask.ca>
2026-03-07 03:41:54 -0600

    Add react-router-dom for routing and wrap App component in BrowserRouter

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Analytics } from '@vercel/analytics/react'
+import { BrowserRouter } from 'react-router-dom'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
     <Analytics />
   </StrictMode>,
 )
